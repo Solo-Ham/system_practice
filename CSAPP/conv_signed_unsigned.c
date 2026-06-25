0d5ef1073a53ed0ec321a90d65d69583469acdd1
Solo-Ham <solomonobonyo74@gmail.com>
2026-06-25 21:53:16 +0300

    conv_signed_unsigned

--- CSAPP/conv_signed_unsigned.c
+++ CSAPP/conv_signed_unsigned.c
@@ -6,4 +6,10 @@ int main()
     short int v = -12345;
     unsigned short uv = (unsigned short) v;
     printf("v = %d, uv = %u\n", v, uv);
+
+    unsigned u = 4294967295u;
+
+    int tu = (int) u;
+/* UMax */
+    printf("u = %u, tu = %d\n", u, tu);
 }
